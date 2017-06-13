9a0d849fa0d7305fcc9cf4a773cb38e7d9f3c8a9
Ian Usher <ian@codeplay.com>
2017-06-13 11:05:55 +0100

    Breaking out entities

---
+++ entities/episode.py
@@ -0,0 +1,28 @@
+from dateutil import parser
+
+class Episode:
+    def __init__(self, episodeData):
+        self.position = episodeData.get('position', -1)
+        self.episode_id = episodeData.get('episode_id', '')
+        self.title = episodeData.get('title', '')
+        self.published = parser.parse(episodeData.get('published', ''))
+        self.link = episodeData.get('link', '')
+        self.description = episodeData.get('description', '')
+
+        self.media_url = episodeData.get('media_url', '')
+        self.media_type = episodeData.get('media_type', '')
+        self.media_length = episodeData.get('media_length', '')
+
+        self.duration = episodeData.get('duration', '')
+        self.keywords = episodeData.get('keywords', [])
+
+        self.guests = []
+
+    def getGuests(self):
+        return sorted([g for g in self.guests if not g.noGuest], key=lambda g: g.name)
+
+    def addGuest(self, guest):
+        if guest is not None:
+            guest.addEpisode(self)
+        if guest is not None and guest not in self.guests:
+            self.guests.append(guest)
